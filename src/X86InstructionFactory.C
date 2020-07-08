e95df89e4290ff0f66eb45fd88e871d76d45a3f5
Allyson Cauble-Chantrenne <allysonc@epanalytics.com>
2020-07-08 23:52:28 +0000

    This commit fixes two errors, both of which involved a 0 being recorded as an address during cache simulation.
    The first bug was with regard to the following instruction:
    `mov %fs:0x28, %rax`. Unfortunately, due to some deprecated code, we were
    only loading the value of `%fs` into the address, instead of calculating the
    address at `%fs:0x28`. The FS segment refers to thread local storage, but for
    some reason, the value of `%fs` is zero (even if we do the instruction
    `lea %fs:0x28, address`). After some gdb, it became clear that after
    `mov %fs:0x28, %rax`, the value in `%rax` is the thread id plus `0x28`. Thus,
    the fix for emitting the address collection for an instruction that uses the
    FS segment of the form `mov %fs:[ADDR], %reg` is to:
    
    ```
    lea [ADDR], %rcx    (where %rcx is our temporary storage register)
    add %fs:0x10, %rcx  (%fs:0x10 holds the thread id)
    ```
    
    The second bug a minor one for the following instruction:
    `rep stosq %rax, %es:(%rdi)`. In this instruction, the operands are implied,
    not explicit. It is only a store (di), but our instruction classifer had it
    as a load and a store (dsi). After making that change and another minor
    bug in emitAddressComputation, the instrumented binary worked just fine.

--- src/X86InstructionFactory.C
+++ src/X86InstructionFactory.C
@@ -191,7 +191,7 @@ X86Instruction* X86InstructionFactory64::emitImmAndReg(uint32_t imm, uint8_t des
 }
 X86Instruction* X86InstructionFactory64::emitImmOrReg(uint32_t imm, uint8_t dest){
     ASSERT(dest < X86_64BIT_GPRS);
-    
+
     uint32_t len = 7;
     char* buff = new char[len];
 
@@ -201,12 +201,35 @@ X86Instruction* X86InstructionFactory64::emitImmOrReg(uint32_t imm, uint8_t dest
     }
     buff[1] = 0x81;
     buff[2] = 0xc8 + (0x01 * (dest % X86_32BIT_GPRS));
-        
+
     uint32_t imm32 = (uint32_t)imm;
     ASSERT(imm32 == (uint32_t)imm && "Cannot use more than 32 bits for the address");
     memcpy(buff+3,&imm32,sizeof(uint32_t));
 
-    return emitInstructionBase(len,buff);    
+    return emitInstructionBase(len,buff);
+}
+
+X86Instruction* X86InstructionFactory64::emitAddTLSOffsetToReg(uint32_t imm, 
+  uint8_t dest){
+    ASSERT(dest < X86_64BIT_GPRS);
+
+    uint32_t len = 9;
+    char* buff = new char[len];
+
+    buff[0] = 0x64;
+    buff[1] = 0x48;
+    if (dest >= X86_32BIT_GPRS){
+        buff[1] += 0x04;
+    }
+    buff[2] = 0x03;
+    buff[3] = 0x04 + (0x08 * (dest % X86_32BIT_GPRS));
+    buff[4] = 0x25;
+
+    uint32_t imm32 = (uint32_t)imm;
+    ASSERT(imm32 == (uint32_t)imm && "Cannot use more than 32 bits for the address");
+    memcpy(buff+5,&imm32,sizeof(uint32_t));
+
+    return emitInstructionBase(len,buff);
 }
 
 X86Instruction* X86InstructionFactory64::emitMoveTLSOffsetToReg(uint32_t imm, uint8_t dest){
@@ -229,6 +252,10 @@ X86Instruction* X86InstructionFactory64::emitMoveTLSOffsetToReg(uint32_t imm, ui
     memcpy(buff+5,&imm32,sizeof(uint32_t));
 
     return emitInstructionBase(len,buff);    
+}
+
+X86Instruction* X86InstructionFactory64::emitAddThreadIdToReg(uint8_t dest){
+    return emitAddTLSOffsetToReg(0x10, dest);
 }
 
 X86Instruction* X86InstructionFactory64::emitMoveThreadIdToReg(uint8_t dest){
@@ -935,11 +962,13 @@ Vector<X86Instruction*>* X86InstructionFactory64::emitAddressComputation(X86Inst
 
         op = instruction->getMemoryOperand();
 
-        if (instruction->GET(pfx_seg)){
-            uint32_t segIdx = instruction->GET(pfx_seg) - UD_R_ES;
-            uint64_t imm = op->getValue();
-            // FIXME ignores offsets
-            (*compInstructions).append(emitMoveSegmentRegToReg(segIdx, dest));
+        if (instruction->GET(pfx_seg) == UD_R_FS) {
+            // The fs register is set to 0, for some reason, even when using 
+            // lea. It is equivalent to the thread id, so lea the address
+            // and then add the thread id
+            (*compInstructions).append(emitLoadEffectiveAddress(op, dest, 
+              true));
+            (*compInstructions).append(emitAddThreadIdToReg(dest));
         } else if (op->GET(base) == UD_R_RIP){
             PRINT_DEBUG_LOADADDR("making lea: mov rip imm");
             uint64_t addr = op->getInstruction()->getProgramAddress() + 
@@ -974,7 +1003,7 @@ Vector<X86Instruction*>* X86InstructionFactory64::emitAddressComputation(X86Inst
                 (*compInstructions).append(emitLoadEffectiveAddress(X86_REG_SI,
                   0, 1, 0, dest, UD_R_DS, true, false, true));
             } else if ((X86InstructionClassifier::getInstructionFormat(
-              instruction) == X86OperandFormat_si || X86InstructionClassifier::
+              instruction) == X86OperandFormat_di || X86InstructionClassifier::
               getInstructionFormat(instruction) == X86OperandFormat_dsi) &&
               impAddrFlag == 0) {
                 (*compInstructions).append(emitLoadEffectiveAddress(X86_REG_DI,
@@ -1079,7 +1108,10 @@ Vector<X86Instruction*>* X86InstructionFactory32::emitAddressComputation(X86Inst
     return compInstructions;
 }
 
-X86Instruction* X86InstructionFactory64::emitLoadEffectiveAddress(OperandX86* op, uint32_t dest){
+// ignore -- if you want to ignore the segment register (like in the case when 
+// the segment register is FS)
+X86Instruction* X86InstructionFactory64::emitLoadEffectiveAddress(OperandX86* 
+  op, uint32_t dest, bool ignoreSeg) {
     ASSERT(dest < X86_64BIT_GPRS && "Illegal register index given");
     ASSERT(op);
 
@@ -1104,13 +1136,25 @@ X86Instruction* X86InstructionFactory64::emitLoadEffectiveAddress(OperandX86* op
     }
 
     bool hasSeg = false;
-    if (op->getInstruction()->GET(pfx_seg)) {
+    if (op->getInstruction()->GET(pfx_seg) && !ignoreSeg) {
         hasSeg = true;
         segReg = op->getInstruction()->GET(pfx_seg);
     }
 
     X86Instruction* lea = emitLoadEffectiveAddress(baseReg, indexReg, scale, 
       value, dest, segReg, hasBase, hasIndex, hasSeg);
+    if (!lea || !op || !lea->getOperand(SRC1_OPERAND)){
+        fprintf(stderr, "emitLoadEffectiveAddress:\n");
+        lea->print();
+    }
+    ASSERT(lea && op && lea->getOperand(SRC1_OPERAND));
+    ASSERT(op->isSameOperand(lea->getOperand(SRC1_OPERAND)) && "The emitted LEA operand does not match the operand given");
+
+    return lea;
+}
+
+X86Instruction* X86InstructionFactory64::emitLoadEffectiveAddress(OperandX86* op, uint32_t dest){
+    X86Instruction* lea = emitLoadEffectiveAddress(op, dest, false); 
     if (!lea || !op || !lea->getOperand(SRC1_OPERAND)){
         fprintf(stderr, "emitLoadEffectiveAddress:\n");
         lea->print();
@@ -1383,22 +1427,54 @@ X86Instruction* X86InstructionFactory64::emitLoadEffectiveAddress(uint32_t
 
     // constant
     else {
-        PRINT_DEBUG_LOADADDR("making lea: const");
+        if (!hasSeg) {
+            PRINT_DEBUG_LOADADDR("making lea: const");
 
-        len = 8;
-        buff = new char[len];
-        if (dest < X86_32BIT_GPRS){
-            buff[0] = 0x48;
+            len = 8;
+            buff = new char[len];
+            if (dest < X86_32BIT_GPRS){
+                buff[0] = 0x48;
+            } else {
+                buff[0] = 0x4c;
+            }
+            buff[1] = 0x8d;
+            buff[2] = 0x04 + 8 * (dest % X86_32BIT_GPRS);
+            buff[3] = 0x25;
+
+            uint32_t addr32 = value;
+            memcpy(buff + 4, &addr32, sizeof(uint32_t));
+            lea = emitInstructionBase(len,buff);
         } else {
-            buff[0] = 0x4c;
-        }
-        buff[1] = 0x8d;
-        buff[2] = 0x04 + 8 * (dest % X86_32BIT_GPRS);
-        buff[3] = 0x25;
+            PRINT_DEBUG_LOADADDR("making lea: const + seg");
+            len = 9;
+            buff = new char[len];
+            if (segReg == UD_R_ES) {
+                buff[0] = 0x26;
+            } else if (segReg == UD_R_CS) {
+                buff[0] = 0x2e;
+            } else if (segReg == UD_R_DS) {
+                buff[0] = 0x3e;
+            } else if (segReg == UD_R_FS) {
+                // Instead of lea-ing the fs segment, lea the address and then 
+                // add to value from emitMoveThreadIdToReg
+                assert(false && "lea will not load fs segment correctly");
+            } else {
+                // Not yet implemented
+                __SHOULD_NOT_ARRIVE;
+            }
+            if (dest < X86_32BIT_GPRS){
+                buff[1] = 0x48;
+            } else {
+                buff[1] = 0x4c;
+            }
+            buff[2] = 0x8d;
+            buff[3] = 0x04 + 8 * (dest % X86_32BIT_GPRS);
+            buff[4] = 0x25;
 
-        uint32_t addr32 = value;
-        memcpy(buff + 4, &addr32, sizeof(uint32_t));
-        lea = emitInstructionBase(len,buff);
+            uint32_t addr32 = value;
+            memcpy(buff + 5, &addr32, sizeof(uint32_t));
+            lea = emitInstructionBase(len,buff);
+        }
     }
 
     ASSERT(lea);
