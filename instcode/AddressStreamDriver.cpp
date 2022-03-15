fe130e9751a7b64466aea946eeeb42b8518e77b8
Emmet Ocampo <emmet.ocampo@epanalytics.com>
2022-03-15 18:55:09 +0000

    Pause Application Wrapper while adding new thread
    When we add a new thread over a certain limit, we trigger a buffer flush which
    then tries to access data structures that are not yet setup yet causing
    segfaults, this pausing and unpausing of the application wrappers around the
    adding thread code fixes that issue

--- instcode/AddressStreamDriver.cpp
+++ instcode/AddressStreamDriver.cpp
@@ -363,6 +363,7 @@ void* AddressStreamDriver::InitializeNewImage(image_key_t* iid,
 
 void* AddressStreamDriver::InitializeNewThread(thread_key_t tid){
     SAVE_STREAM_FLAGS(cout);
+    PauseApplicationWrappers();
     if (allData){
         if(dynamicPoints->IsThreadedMode())
             allData->AddThread(tid);
@@ -377,6 +378,7 @@ void* AddressStreamDriver::InitializeNewThread(thread_key_t tid){
           MetasimError_NoThread);
     }
 
+    UnpauseApplicationWrappers();
     RESTORE_STREAM_FLAGS(cout);
     return NULL;
 }
