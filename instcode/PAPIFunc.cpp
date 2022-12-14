64ff1d7096e908b8d30459c63d17b6ec0a08f164
Allyson Cauble-Chantrenne <allysonc@epanalytics.com>
2022-12-14 19:26:38 +0000

    Added configuration option "--with-libuwind=<path>" for epa-pebil-tools

--- instcode/PAPIFunc.cpp
+++ instcode/PAPIFunc.cpp
@@ -245,12 +245,12 @@ extern "C"
           // No error checking to minimize overhead
           int error = PAPI_read(eventSet, counters->tmpValues[funcIndex]);
       
-          DEBUG({
-              if (error != PAPI_OK){
-                  fprintf(stderr, "Error reading the values!\n");
-                  exit(1);
-              }
-          });
+          #ifdef PEBIL_DEBUG
+          if (error != PAPI_OK){
+              fprintf(stderr, "Error reading the values!\n");
+              exit(1);
+          }
+          #endif
   
           // associate the counter values to all the active functions
           for (std::set<int>::iterator it = counters->activeFunctions.begin();
@@ -333,12 +333,12 @@ extern "C"
           // No error checking to minimize overhead
           int error = PAPI_start(eventSet);
 
-          DEBUG({
-              if (error != PAPI_OK) {
-                  fprintf(stderr, "Error in PAPI start!\n");
-                  exit(1);
-              }
-          });
+          #ifdef PEBIL_DEBUG
+          if (error != PAPI_OK) {
+              fprintf(stderr, "Error in PAPI start!\n");
+              exit(1);
+          }
+          #endif
 
           // indicate that the measurements have started 
           counters->papiMeasurementsStarted = 1;
@@ -346,12 +346,12 @@ extern "C"
           // else reset the counters (again doing it without the check)
           int error = PAPI_reset(eventSet);
 
-          DEBUG({
-              if (error != PAPI_OK) {
-                  fprintf(stderr, "Error in PAPI reset!\n");
-                  exit(1);
-              }
-          });
+          #ifdef PEBIL_DEBUG
+          if (error != PAPI_OK) {
+              fprintf(stderr, "Error in PAPI reset!\n");
+              exit(1);
+          }
+          #endif
       }
       counters->eventSet = eventSet;
       counters->functionTimerLast[funcIndex] = read_timestamp_counter();
